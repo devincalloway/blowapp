576608a184a498dbe21fb72bfe9c79cb0ab36179
ital <exodusrasta@gmail.com>
2012-07-05 18:44:48 -0700

    Tags Controller Removed

--- app/controllers/tags_controller.rb
+++
@@ -1,8 +0,0 @@
-class TagsController < ApplicationController
-
-  def show
-    @page_title = "#{ t :records_with_tag } #{ params[:id] }"
-    session[:admin] ? @posts = Post.tagged_with(params[:id]).by_date : @posts = Post.published.tagged_with(params[:id]).by_date
-  end
-
-end
